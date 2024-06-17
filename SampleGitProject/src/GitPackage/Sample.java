afd6c7c72552afdd19880261dd6190a123155d47
Admin <Admin@DESKTOP-EHNLCK4>
2024-06-17 19:02:01 +0530

    Added Sample class

---
+++ SampleGitProject/src/GitPackage/Sample.java
@@ -0,0 +1,13 @@
+package GitPackage;
+
+public class Sample {
+
+	public static void main(String[] args) 
+	{
+	
+		     System.out.println("**Hellooo Ankita**");
+		     System.out.println("HOW R U");
+    }
+}
+
+
