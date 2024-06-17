0da6716f6805ac7ee6a036c9cbba3ce431ad4de5
Admin <Admin@DESKTOP-EHNLCK4>
2024-06-17 19:52:25 +0530

    Again master branch updated

--- SampleGitProject/src/GitPackage/Sample.java
+++ SampleGitProject/src/GitPackage/Sample.java
@@ -7,7 +7,7 @@ public class Sample {
 	
 		     System.out.println("**Hellooo Ankita**");
 		     System.out.println("HOW R U");
-		     System.out.println("TestBranch started");
+		     
 
     }
 }
